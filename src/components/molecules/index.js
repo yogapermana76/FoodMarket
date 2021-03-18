2469d022f0af2d85a66ae7f8766ca553b1b20895
Yoga Permana <yoga@xcidic.com>
2021-03-18 19:28:15 +0700

    custom tab navigator

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,4 @@
 import Header from './Header';
+import ButtomNavigator from './ButtonNavigator';
 
-export { Header };
+export { Header, ButtomNavigator };
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,14 @@ import {
   SplashScreen,
   SuccessSignUp,
 } from '../pages';
+import { ButtomNavigator } from '../components';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={props => <ButtomNavigator {...props} />}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Order" component={Order} />
       <Tab.Screen name="Profile" component={Profile} />
