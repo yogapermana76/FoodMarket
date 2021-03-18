c69013b43bc31b53cb55b6b14987c18765c6f2c9
Yoga Permana <yoga@xcidic.com>
2021-03-18 17:55:39 +0700

    feat(SignUpAddress): slicing SignUpAddress screen

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -27,7 +27,10 @@ const SignUp = ({ navigation }) => {
         <Gap height={16} />
         <TextInput label="Password" placeholder="Type your password" />
         <Gap height={24} />
-        <Button text="Continue" />
+        <Button
+          text="Continue"
+          onPress={() => navigation.navigate('SignUpAddress')}
+        />
       </View>
     </View>
   );
---
+++ src/pages/SignUpAddress/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Gap, Header, Select, TextInput } from '../../components';
+
+const SignUpAddress = () => {
+  return (
+    <View style={styles.page}>
+      <Header
+        title="Address"
+        subTitle="Make sure it’s valid"
+        onBack={() => navigation.goBack()}
+      />
+      <View style={styles.container}>
+        <TextInput label="Phone No." placeholder="Type your phone number" />
+        <Gap height={16} />
+        <TextInput label="Address" placeholder="Type your address" />
+        <Gap height={16} />
+        <TextInput label="House No." placeholder="Type your house number" />
+        <Gap height={16} />
+        <Select label="City" />
+        <Gap height={24} />
+        <Button text="Sign Up Now" />
+      </View>
+    </View>
+  );
+};
+
+export default SignUpAddress;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: 'white',
+    paddingVertical: 26,
+    paddingHorizontal: 24,
+    marginTop: 24,
+    flex: 1,
+  },
+});
