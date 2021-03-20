ab6d61a43eff0958d30894ecd88b7d1cec4e70e9
Yoga Permana <yoga@xcidic.com>
2021-03-20 22:47:14 +0700

    install axios

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,7 @@
+import Axios from 'axios';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Header } from '../../components';
-import { Button, Gap, TextInput } from '../../components';
+import { Button, Gap, Header, TextInput } from '../../components';
 import { useForm } from '../../utils';
 
 const SignIn = ({ navigation }) => {
@@ -9,6 +9,18 @@ const SignIn = ({ navigation }) => {
     email: '',
     password: '',
   });
+
+  const onSubmit = async () => {
+    try {
+      const res = await Axios.post(
+        'http://foodmarket-backend.buildwithangga.id/api/login',
+        form,
+      );
+      console.log(res, 'success');
+    } catch (error) {
+      console.log(error, 'error');
+    }
+  };
   return (
     <View style={styles.page}>
       <Header title="Sign In" subTitle="Find your best ever meal" />
@@ -28,7 +40,7 @@ const SignIn = ({ navigation }) => {
           secureTextEntry
         />
         <Gap height={24} />
-        <Button text="Sign In" />
+        <Button text="Sign In" onPress={onSubmit} />
         <Gap height={12} />
         <Button
           text="Create New Account"
